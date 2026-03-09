8295d2dff3200603ade52c5cbee8c3c1e726ffeb
h-mutumwinka <h.mutumwink@alustudent.com>
2026-03-09 11:03:37 +0200

    updated

--- kigali_city_directory/lib/screens/web_demo_wrapper.dart
+++ kigali_city_directory/lib/screens/web_demo_wrapper.dart
@@ -3,19 +3,17 @@ import 'package:flutter/foundation.dart' show kIsWeb;
 import 'navigation_screen.dart';
 import 'auth_wrapper.dart';
 
-/// Wrapper that bypasses auth on web for demo purposes
-/// On mobile (Android/iOS), uses full authentication
 class WebDemoWrapper extends StatelessWidget {
   const WebDemoWrapper({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    // On web: Skip authentication for demo
+    
     if (kIsWeb) {
       return const MainNavigationScreen();
     }
     
-    // On mobile: Use full authentication
+    
     return const AuthWrapper();
   }
 }
